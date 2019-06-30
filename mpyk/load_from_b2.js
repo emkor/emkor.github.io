33c4a0611f41aa7d49b98ee93cc7fd19c591745c
emkor93 <emkor93@gmail.com>
2019-06-30 18:02:10 +0200

    switched to GETting listing file without authorization first

--- mpyk/load_from_b2.js
+++ mpyk/load_from_b2.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
     let b2Token = "";
     let b2DownloadUrl = "";
+    let b2ListingFileUrl = "https://f001.backblazeb2.com/file/mpk-wroclaw/listing.json";
     let b2AuthUrl = "https://api.backblazeb2.com/b2api/v5/b2_authorize_account";
     let b2Bucket = "mpk-wroclaw";
     let b2IdKey = "001a56c121eaece0000000004:K001N+B5pe3hbaSZQSddFVP9WztLoEI";
     let headers = new Headers();
     headers.set("Authorization", "Basic " + btoa(b2IdKey));
 
-    fetch(b2AuthUrl, {method: "GET", headers: headers})
+    fetch(b2ListingFileUrl)
         .then(resp => resp.json())
         .then(jsonResp => {
+            console.log("Success:", jsonResp);
             console.log("Success:", JSON.stringify(jsonResp));
-            b2Token = jsonResp.authorizationToken;
-            b2DownloadUrl = jsonResp.downloadUrl;
         })
         .catch(error => console.error("Error:", error));
 
