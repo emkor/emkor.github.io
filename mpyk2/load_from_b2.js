29a71b038f5b1a9687e0c0bc05de3c6292af8fb2
emkor93 <emkor93@gmail.com>
2019-09-01 16:20:00 +0200

    added debug code for parsing CSV

---
+++ mpyk2/load_from_b2.js
@@ -0,0 +1,48 @@
+function bytesToSize(bytes) {
+    var sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return '0';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+}
+
+function timestampToUtcString(timeStamp) {
+    var newDate = new Date();
+    newDate.setTime(timeStamp);
+    return newDate.toISOString();
+}
+
+$(document).ready(function () {
+    let b2ApiHost = "https://f001.backblazeb2.com";
+    fetch(b2ApiHost + "/file/mpk-wroclaw/listing.csv")
+        .then(resp => {
+            if (resp.status === 200) {
+                return resp.text()
+            } else {
+                return Promise.reject(new Error(resp.statusText))
+            }
+        })
+        .then(resp => {
+            console.log(resp);
+            let htmlElems = [];
+            let dlByIdUrl = "/b2api/v1/b2_download_file_by_id?fileId=";
+            let sortedFiles = resp.sort((a, b) => b.uploadTimestamp - a.uploadTimestamp);
+            for (var file of sortedFiles) {
+                if (file.fileName !== "listing.json") {
+                    let fileDlUrl = b2ApiHost + dlByIdUrl + file.fileId;
+                    htmlElems.push("<tr>" +
+                        "<td><a href='" + fileDlUrl + "'>" + file.fileName + "</a></td>" +
+                        "<td>" + timestampToUtcString(file.uploadTimestamp) + "</td>" +
+                        "<td>" + bytesToSize(file.contentLength) + "</td>" +
+                        "</tr>")
+                }
+            }
+            $("#download table tbody").append(htmlElems.join(""));
+            $("#download #loadInProgressInfo").addClass("hidden");
+            $("#download table").removeClass("hidden");
+        })
+        .catch(error => {
+            $("#download #loadInProgressInfo").addClass("hidden");
+            $("#download #loadFailedInfo").removeClass("hidden");
+            console.error("Error:", error)
+        });
+});
